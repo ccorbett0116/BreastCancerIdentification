77b5ac644f4b55aecc1dabc7100f4158d4379437
ccorbett0116 <100178889+ccorbett0116@users.noreply.github.com>
2025-03-19 05:00:25 -0400

    added critical code

--- main.py
+++ main.py
@@ -10,16 +10,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pydicom
+import tensorboard
 
 #test cuda
 if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    print('Using device:', device)
-    print()
-
-    #Additional Info when using cuda
+    print('Using device: ', end='')
     if device.type == 'cuda':
-        print(torch.cuda.get_device_name(0))
-        print('Memory Usage:')
-        print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
-        print('Cached:   ', round(torch.cuda.memory_reserved(0)/1024**3,1), 'GB')
+        print(torch.cuda.get_device_name(0))  # Flexes the 5080
+    else:
+        print(device)
+
